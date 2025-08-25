aee261cd47da7103f1266d5d9456c53ae4187137
Kio281 <6731503048@lamduan.mfu.ac.th>
2025-08-25 09:08:00 +0000

    done

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -3,17 +3,40 @@ package th.mfu;
 import java.util.Date;
 
 public class Concert {
+  private static long counter = 0;
 
-    //TODO: add attributes
+    private Long id;
+    private String title;
+    private String performer;
+    private String date;
+    private String description;
 
+    // Default constructor
     public Concert() {
+        this.id = ++counter;
     }
-    public Concert(String title, String description) {
-        //TODO: set attributes
 
+    // Parameterized constructor
+    public Concert(String title, String description) {
+        this.id = ++counter;
+        this.title = title;
+        this.description = description;
     }
- 
-    //TODO: add getters and setters for all attributes
-    // You can use Source action/generate getter setter function
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id){this.id = id;}
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getPerformer() { return performer; }
+    public void setPerformer(String performer) { this.performer = performer; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
 }
